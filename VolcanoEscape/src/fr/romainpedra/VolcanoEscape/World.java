be5dd7d9d1160db723ed10b9525f9e501b4d6c92
bananabipac <dossantos.jordan94@gmail.com>
2013-02-19 14:42:38 +0100

    Add Fucking World.java and the Wall.java biatch

---
+++ VolcanoEscape/src/fr/romainpedra/VolcanoEscape/World.java
@@ -0,0 +1,28 @@
+package fr.romainpedra.VolcanoEscape;
+
+import java.util.ArrayList;
+
+public class World {
+
+	public ArrayList<Rock> rocks; //liste des rochers present sur le terrain
+	public Player player; //Objet Joueur
+	public float speedScroll; //Vitesse de defilement du terrain
+	private ArrayList<Wall> WallRight; //paroies de droite du terrain
+	private ArrayList<Wall> wallleft; //paroies de gauche du terrain
+	
+	private int height;
+	private int width;
+	
+	
+	public World(int speedScroll, Player player)
+	{
+		this.speedScroll =  speedScroll;
+		this.rocks = new ArrayList<Rock>();
+		this.player = player;
+	}
+	
+	public void UpdateWorld()
+	{
+		
+	}
+}
