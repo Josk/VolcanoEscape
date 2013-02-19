145199b6d1a948d104e6caf9d9107813533add15
PEDRA <romain.pedra@gmail.fr>
2013-02-19 12:01:12 +0100

    FirstCommit

---
+++ VolcanoEscape-desktop/src/fr/romainpedra/VolcanoEscape/Main.java
@@ -0,0 +1,16 @@
+package fr.romainpedra.VolcanoEscape;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "VolcanoEscape";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new VolcanoEscape(), cfg);
+	}
+}
