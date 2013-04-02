04075be782a15713e5aad0baddf6b74283488e4a
bananabipac <dossantos.jordan94@gmail.com>
2013-04-02 16:00:19 +0200

    added lava sprite system

--- VolcanoEscape/src/fr/romainpedra/VolcanoEscape/Lava.java
+++ VolcanoEscape/src/fr/romainpedra/VolcanoEscape/Lava.java
@@ -1,97 +1,79 @@
 package fr.romainpedra.VolcanoEscape;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.esotericsoftware.tablelayout.BaseTableLayout.Debug;
 
 public class Lava extends Actor{
-	TextureRegion rgn = new TextureRegion();
-	TextureRegion rgn2 = new TextureRegion();
-	TextureRegion rgn3 = new TextureRegion();
+	ArrayList<TextureRegion> lavaTexture;
 	float x;
 	float y;
 	float width;
 	float height;
-	private float drawX;
-	private float drawY;
-	private float speedX;
-	private float speedY;
-	private float rgn1X;
-	private float rgn2X;
-	private float rgn1Y;
-	private float rgn2Y;
-	private boolean invertX;
-	private boolean invertY;
+	float speedSprite;
+	private int indexSprite;
+	private float timeTmp;
+	private boolean invert;
+	
 	
 	public Lava(Stage stage,float x, float y, float width, float height){
-		rgn = new TextureRegion(Assets.get().lave);
-		rgn2 = new TextureRegion(Assets.get().lave);
-		rgn3 = new TextureRegion(Assets.get().lave);
+		this.indexSprite = 0;
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.speedSprite =0.8f;
+		this.timeTmp = 0;
 		setSize(this.width,this.height);
 		setOrigin(this.width/2,this.height/2);
-		this.drawX = 30f;
-		this.drawY = 50f;
-		this.rgn1X = 0;
-		this.rgn2X = this.drawX;
-		this.rgn1Y = 0;
-		this.rgn2Y = this.drawY;
-		this.invertX = false;
-		this.invertY = false;
-		this.speedX = 0.1f;
-		this.speedY = 0.20f;
-		
 		setPosition(this.x,this.y);
+		this.invert = false;
+		
+		this.indexSprite = 0;
+		
+		lavaTexture = new ArrayList<TextureRegion>();
+		
+		lavaTexture.add(new TextureRegion(Assets.get().lave1));
+		lavaTexture.add(new TextureRegion(Assets.get().lave2));
+		lavaTexture.add(new TextureRegion(Assets.get().lave3));
+		lavaTexture.add(new TextureRegion(Assets.get().lave4));
+		lavaTexture.add(new TextureRegion(Assets.get().lave5));
+		lavaTexture.add(new TextureRegion(Assets.get().lave6));
+		
 	}
 	
 	@Override
 	public void draw(SpriteBatch batch, float parentAlpha){
 		
-		if(this.invertX)
+		this.timeTmp += 0.1f;
+		if(this.timeTmp >= this.speedSprite)
 		{
-			this.rgn1X -= this.speedX;
-			this.rgn2X += this.speedX;
-			if(this.rgn1X <= 0)
+			this.timeTmp = 0;
+			if(invert)
 			{
-				this.invertX = false;
+				this.indexSprite --;
+				if(indexSprite < 0)
+				{
+					this.indexSprite = 1;
+					invert = false;
+				}
 			}
-		}
-		else
-		{
-			this.rgn1X += this.speedX;
-			this.rgn2X -= this.speedX;
-			if(this.rgn1X >= this.drawX)
+			else
 			{
-				this.invertX = true;
+				this.indexSprite ++;
+				if(indexSprite == this.lavaTexture.size())
+				{
+					this.indexSprite = this.lavaTexture.size() -2;
+					invert = true;
+				}
 			}
+			
 		}
-		
-		if(this.invertY)
-		{
-			this.rgn1Y -= this.speedY;
-			this.rgn2Y += this.speedY;
-			if(this.rgn1Y <= 0)
-			{
-				this.invertY = false;
-			}
-		}
-		else
-		{
-			this.rgn1Y += this.speedY;
-			this.rgn2Y -= this.speedY;
-			if(this.rgn1Y >= this.drawY)
-			{
-				this.invertY = true;
-			}
-		}
-		
-		batch.draw(rgn2,getX() + this.rgn2X ,getY() + this.rgn2Y,getOriginX(),getOriginY(),getWidth(),getHeight(),getScaleX(),getScaleY(),getRotation());
-		batch.draw(rgn3,getX() + this.rgn1X ,getY(),getOriginX(),getOriginY(),getWidth(),getHeight(),getScaleX(),getScaleY(),getRotation());
-		batch.draw(rgn,getX() ,getY(),getOriginX(),getOriginY(),getWidth(),getHeight(),getScaleX(),getScaleY(),getRotation());
+		batch.draw(lavaTexture.get(this.indexSprite),getX() ,getY(),getOriginX(),getOriginY(),getWidth(),getHeight(),getScaleX(),getScaleY(),getRotation());
 	}
 }
